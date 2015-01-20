b0d96378c1e974ee26681a9a4d6087c09ab3924a
Alex Robertson <alexrson@gmail.com>
2015-01-20 10:15:29 -0800

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-  alert("hello");
+  document.getElementsByTagName("td")[0].setAttribute("bgcolor", "2CB4AC");
+  document.getElementsByTagName("table")[0].setAttribute("bgcolor", "F6F6F6");
+  document.getElementsByTagName("a")[1].innerText = "Alex is awesome";
 });
